9789ffa7ec5d3d48bf970bf842be6aebae18d463
IcaroRaphael <icaro2551@gmail.com>
2025-06-17 18:51:04 -0300

    Exercicio06

--- Exercicios/Exercicio06/Program.cs
+++ Exercicios/Exercicio06/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using Exercicio06;
+
+Aluno a1 = new Aluno("Icaro", "123456789");
+a1.RegistrarNotas(7.0, 8.0, 9.0);
+Console.WriteLine($"Média: {a1.CalcularMedia():f1}");
+Console.WriteLine($"Aprovado: {a1.VerificarAprovacao()}");
